e4b5baa2acc6234a6cd96bc464e6356f56e41864
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-01-31 01:21:35 +0500

    create signup api routes and controller

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then( () => {
 
 const app = express();
 
+app.use(express.json());
 
 app.listen(3000, () => console.log('Server started on port 3000!!'));
 
@@ -22,3 +24,4 @@ app.listen(3000, () => console.log('Server started on port 3000!!'));
 //})
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute); 
