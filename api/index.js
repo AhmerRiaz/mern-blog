58e90ad6ed72837858602d36a5ea52e13ad5c173
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-01 11:51:04 +0500

    add functionality to the signup page

--- api/index.js
+++ api/index.js
@@ -1,39 +1,39 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
+    import express from 'express';
+    import mongoose from 'mongoose';
+    import dotenv from 'dotenv';
+    import userRoute from './routes/user.route.js';
+    import authRoute from './routes/auth.route.js';
 
-dotenv.config();
+    dotenv.config();
 
 
-mongoose.connect(process.env.MONGO).then( () => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.log(error);
-})
-
-const app = express();
-
-app.use(express.json());
-
-app.listen(3000, () => console.log('Server started on port 3000!!'));
-
-// app.get('/test', (req, res) => {
-   // res.json({'message': 'Hello World!'});
-//})
-
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute); 
-
-app.use((err, req, res, next) => { 
-
-    const statusCode = err.status || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success:false,
-        message,
-        statusCode,
-
+    mongoose.connect(process.env.MONGO).then( () => {
+        console.log('Connected to MongoDB');
+    }).catch((error) => {
+        console.log(error);
     })
-})
+
+    const app = express();
+
+    app.use(express.json());
+
+    app.listen(3000, () => console.log('Server started on port 3000!!'));
+
+    // app.get('/test', (req, res) => {
+    // res.json({'message': 'Hello World!'});
+    //})
+
+    app.use('/api/user', userRoute);
+    app.use('/api/auth', authRoute); 
+
+    app.use((err, req, res, next) => { 
+
+        const statusCode = err.status || 500;
+        const message = err.message || 'Internal Server Error';
+        res.status(statusCode).json({
+            success:false,
+            message,
+            statusCode,
+
+        })
+    })
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,7 +3,7 @@ import { signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/signup', signUp )
+router.post('/signup', signUp);
 
 
 
