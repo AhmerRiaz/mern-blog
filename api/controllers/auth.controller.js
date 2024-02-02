51c1f322e839266655fc1f74af54f60880954100
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-02 23:55:25 +0500

    create signin api route, jwt token, cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signUp = async (req, res, next) => {
@@ -26,4 +27,35 @@ export const signUp = async (req, res, next) => {
     next(error);    
    }
    
+   }
+
+   export const signIn = async (req, res, next) => {
+       const { email, password } = req.body;
+
+       if(!email || !password || email === '' || password === '') {
+           return next(errorHandler(400, 'Please provide all the fields'));
+       }
+
+        try {
+            const validUser = await User.findOne({ email });
+       if(!validUser) {
+           return next(errorHandler(404, 'User not found'));
+       }
+       const validPassword = bcryptjs.compareSync(password, validUser.password);
+       if(!validPassword) {
+           return next(errorHandler(400, 'Invalid credentials'));
+       }
+       const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+
+       const {password: pass, ...rest} = validUser._doc
+
+       res.status(200)
+       .cookie('access_token', token, {
+           httpOnly: true,
+       }).json(rest);
+            
+        } catch (error) {
+            next(error);
+        }
+       
    }
