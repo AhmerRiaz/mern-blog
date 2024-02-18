836ef2c6c4debe1918b3d680fa71bec0219710fa
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-18 11:24:31 +0500

    complete a create post ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -9,7 +9,7 @@ import { userUpdateStart, userUpdateSuccess, userUpdateFailure,
   userDeleteStart, userDeleteSuccess, userDeleteFailure, userSignOutStart, userSignOutSuccess, userSignOutFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux"
 import { HiOutlineExclamationCircle } from 'react-icons/hi'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 export default function DashProfile() {
   const navigate = useNavigate();
@@ -226,6 +226,17 @@ useEffect(() => {
         <Button type="submit" gradientDuoTone={"purpleToBlue"} outline disabled={loading || imageFileUploading}>
           {loading ? 'Updating...' : 'Update Profile'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+          <Button
+          type="button"
+          gradientDuoTone={"purpleToPink"}
+          className="w-full"
+          > 
+          Create a Post 
+          </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">Delete Account</span>
