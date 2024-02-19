2b4618a5acc1182ecd389b3837b0804f0af479c1
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-19 11:33:00 +0500

    add a create a post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,32 @@
+import { errorHandler } from "../utils/error.js";
+import Post from "../model/post.model.js";
+
+
+export const createPost = async (req, res, next) => {
+    console.log(req.user);
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all the fields"));
+  }
+
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user._id,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
