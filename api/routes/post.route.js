e27d7c6b2f5d9394d7cca86de90abb55c678f569
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-27 12:19:10 +0500

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { createPost } from '../controllers/post.controller.js';
+import { getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create-post', verifyToken, createPost);
+router.get('/getposts', getposts);
 
 export default router;
