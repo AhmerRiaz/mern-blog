c2c555a27bcdf16441c9e0f348ecb195e0e698a8
Ahmer Riaz <ahmerriaz95@gmail.com>
2024-02-27 11:53:38 +0500

    add post section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,69 +1,81 @@
-import { useState, useEffect } from 'react'
-import { useLocation, Link } from 'react-router-dom'
-import { Sidebar, SidebarItemGroup } from 'flowbite-react'
-import { HiUser, HiArrowSmRight } from 'react-icons/hi'
-import { useDispatch } from 'react-redux'
-import { userSignOutStart, userSignOutSuccess, userSignOutFailure } from '../redux/user/userSlice'
-
-
+import { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Sidebar, SidebarItem, SidebarItemGroup } from "flowbite-react";
+import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {
+  userSignOutStart,
+  userSignOutSuccess,
+  userSignOutFailure,
+} from "../redux/user/userSlice";
 
 export default function DashSidebar() {
-  const dispatch = useDispatch()
-    const location = useLocation()
-    const [tab, setTab] = useState('');
-    useEffect(() => {
-        const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
-        if (tabFromUrl) {
-          setTab(tabFromUrl);
-        }
-      }, [location.search]);
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
 
-      const handleSignOut = async () => {
-        try {
-         dispatch(userSignOutStart());
-         const res = await fetch('/api/user/signout', {
-           method: 'POST',
-         })
-         const data = await res.json();
-         if(!res.ok) {
-           dispatch(userSignOutFailure(data.message));
-         } else {
-           dispatch(userSignOutSuccess(data));
-         }
-         
-        } catch (error) {
-         dispatch(userSignOutFailure(error.message));
-        }
+  const handleSignOut = async () => {
+    try {
+      dispatch(userSignOutStart());
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(userSignOutFailure(data.message));
+      } else {
+        dispatch(userSignOutSuccess(data));
       }
+    } catch (error) {
+      dispatch(userSignOutFailure(error.message));
+    }
+  };
 
-    return (
-     
-    <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            <Link to={'/dashboard?tab=profile'}>
-            <Sidebar.Item active={tab === 'profile'} 
-            icon={HiUser}
-            label={'User'}
-            labelColor='dark'
-            as='div'
-            >
-            Profile
-            </Sidebar.Item>
-            
-            </Link>
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to={"/dashboard?tab=profile"}>
             <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={currentUser.isAdmin ? "Admin" : "User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+          <Link to={"/dashboard?tab=posts"}>
+          <Sidebar.Item 
+            active={tab === "posts"}
+            icon={HiDocumentText}
+            as="div"
+          >
+            Posts
+            </Sidebar.Item>
+        </Link>  
+          )}
+          
+          <Sidebar.Item
             icon={HiArrowSmRight}
-            className='cursor-pointer'
+            className="cursor-pointer"
             onClick={handleSignOut}
           >
             Sign Out
           </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
-
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-    
-  )
+  );
 }
